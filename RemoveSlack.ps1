22cb6838443cd9e2acb674225ad5439c402a9240
rebo12 <62980057+rebo12@users.noreply.github.com>
2020-07-23 19:07:54 -0400

    Update RemoveSlack.ps1

--- RemoveSlack.ps1
+++ RemoveSlack.ps1
@@ -3,29 +3,38 @@
 # Uncomment all commented lines from the body of the script before running
 
 Read-Host -Prompt "Press Enter to remove all Slack files from all users"
-
 Read-Host -Prompt "ARE YOU SURE?? (Enter to continue)"
 
+If(Test-Path -Path "C:\Program Files\Slack"){
+    Remove-Item -path "C:\Program Files\Slack" -recuse
+    } Else{
+    Write-Host "C:\Program Files\Slack was not found"
+    }
+If(Test-Path -Path "C:\Program Files\Slack"){
+    Remove-Item -path "C:\Program Files\Slack Deployment" -recuse
+    } Else{
+    Write-Host "C:\Program Files\Slack Deployment was not found"
+    }
 
 $folderS = Get-ChildItem –Path "C:\Users" | where-object {$_.Psiscontainer -eq "True"} | select-object name
-
-
 foreach ($folder in $folderS) {
 $path1 = "C:\Users\" + $folder.Name + "\AppData\Local\Slack"
 $path2 = "C:\Users\" + $folder.Name + "\AppData\Roaming\Slack"
 
 If (Test-Path -Path $path1) {
-#    Remove-Item –path $path1 –recurse
+    Remove-Item –path $path1 –recurse
     Write-Host $path2 "was removed"
     }Else {
     Write-Host $path2 "was not found"
     }
 
 If (Test-Path -Path $path1) {
-#    Remove-Item –path $path2 –recurse
+    Remove-Item –path $path2 –recurse
     Write-Host $path2 "was removed"
     } Else {
     Write-Host $path2 "was not found"
     }
 
 }
+
+Read-Host
