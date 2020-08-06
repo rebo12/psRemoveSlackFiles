98dbf710715b7e951cec165eb0d91cd6d89876d4
rebo12 <62980057+rebo12@users.noreply.github.com>
2020-08-06 15:01:14 -0400

    Update RemoveSlack.ps1

--- RemoveSlack.ps1
+++ RemoveSlack.ps1
@@ -1,22 +1,24 @@
-﻿# Reggie Scerbo - rscerbo@databridgesites.com - 7/23/20
+# Reggie Scerbo - rscerbo@databridgesites.com - 7/23/20
 # Remove Slack directories from all users on a PC
 # Uncomment all commented lines from the body of the script before running
 
 Read-Host -Prompt "Press Enter to remove all Slack and MSTeams files from all users"
-Read-Host -Prompt "ARE YOU SURE?? (Enter to continue)"
 
+# remove slack from program files
 If(Test-Path -Path "C:\Program Files\Slack"){
     Remove-Item -path "C:\Program Files\Slack" -recurse
     Write-Host "C:\Program Files\Slack was removed"
     } Else{
     Write-Host "C:\Program Files\Slack was not found"
     }
+# remove slack deployment from program files
 If(Test-Path -Path "C:\Program Files\Slack Deployment"){
     Remove-Item -path "C:\Program Files\Slack Deployment" -recurse
     Write-Host "C:\Program Files\Slack Deployment was removed"
     } Else{
     Write-Host "C:\Program Files\Slack Deployment was not found"
     }
+# remove teams installer from program files (x86)
 If(Test-Path -Path "C:\Program Files (x86)\Teams Installer"){
     Remove-Item -path "C:\Program Files (x86)\Teams Installer" -recurse
     Write-Host "C:\Program Files (x86)\Teams Installer was removed"
@@ -24,19 +26,21 @@ If(Test-Path -Path "C:\Program Files (x86)\Teams Installer"){
     Write-Host "C:\Program Files (x86)\Teams Installer was not found"
     }
 
+# make an array of all the 7 file paths that need to be removed from each user account
 $path = @(0,0,0,0,0,0,0,0,0,0)
-
 $folderS = Get-ChildItem –Path "C:\Users" | where-object {$_.Psiscontainer -eq "True"} | select-object name
 foreach ($folder in $folderS) {
 $path[0] = "C:\Users\" + $folder.Name + "\AppData\Local\Slack"
-$path[1] = "C:\Users\" + $folder.Name + "\AppData\Local\Microsoft\Teams*"
+$path[1] = "C:\Users\" + $folder.Name + "\AppData\Local\Teams"
 $path[2] = "C:\Users\" + $folder.Name + "\AppData\Local\SquirrelTemp"
 $path[3] = "C:\Users\" + $folder.Name + "\AppData\Local\Microsoft\Teams*"
 $path[4] = "C:\Users\" + $folder.Name + "\AppData\Roaming\Microsoft\Teams"
 $path[5] = "C:\Users\" + $folder.Name + "\AppData\Roaming\Microsoft Teams"
-$path[6] = "C:\Users\" + $folder.Name + "\Desktop\Slack.lnk"
+$path[6] = "C:\Users\" + $folder.Name + "\AppData\Roaming\Slack"
+$path[7] = "C:\Users\" + $folder.Name + "\Desktop\Slack.lnk"
 
-For($i = 0; $i -le 6; $i ++) {
+# loop through each user account and remove the file paths from the array
+For($i = 0; $i -le 7; $i ++) {
     If (Test-Path -Path $path[$i]) {
         Remove-Item –path $path[$i] –recurse
         Write-Host $path[$i] "was removed"
